d874b79236ebc4ece40411d76e9f7cebac281905
anyieni <govygov@gmail.com>
2017-11-22 19:51:24 +0300

    add test for successful user login

--- tests/test_user.py
+++ tests/test_user.py
@@ -8,5 +8,11 @@ class Test_user(unittest.TestCase):
 
 	def test_newuser(self):
 		self.payload=dict(username="govan", password="govan1")
-		response= self.client.post("/signup", data=self.payload, follow_redirects=True)
+		response=self.client.post("/signup", data=self.payload, follow_redirects=True)
+		self.assertEqual(response.status_code, 200)
+
+	def test_user_login(self):
+		self.payload=dict(username="govan", password="govan1")
+		self.client.post("/signup", data=self.payload)
+		response=self.client.post("/login", data=self.payload, follow_redirects=True)
 		self.assertEqual(response.status_code, 200)
